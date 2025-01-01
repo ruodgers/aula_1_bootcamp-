9da3f4bf92c0b89974cdc37c9bbf800d5b680971
ruodgers <ruodgers@xgs0r.onmicrosoft.com>
2025-01-01 16:26:24 -0300

    salvando o exemplo 02

--- exemplo_02.py
+++ exemplo_02.py
@@ -6,7 +6,24 @@ print(3) #pode ser texto ou número#
 print(3 + 4) #pode ser opereações#
 
 
-print("Olá" + " " "Pessoal") #pode concatenar varáiveis do mesmo tippo#
+#print("Olá" + " " "Pessoal") #pode concatenar varáiveis do mesmo tipo#
 
 #COMANDO INPUT#
-print("Olá" + " " + input(" Digite o seu nome: ") + "!")
+print("Olá" + " " + input(" Digite o seu nome: ") + "!")#
+
+#tipos de varíavel"#
+numero = 3 # int
+nome_de_usuario = "Roger" # string
+numero_decimal = 4.78 # float
+verdadeiro  = True or False # bool
+
+#Comando type serve apra descobrir o tipo  de variável
+print(type(numero))
+
+#nomes de variáveis sempre ficam em minúsculo
+#python é case sensitive, então Variavel é diferente de variavel
+#senhas sempre maiúsculas
+SENHA = 5616493464
+#undesrcore na frente diz que a variável é secreta
+_SENHA2 = 56564946464
+#nomes de comandos do python não podem ser variaveis
