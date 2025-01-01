339d332bfa310ecef359fe8abb6eb791cf247480
ruodgers <ruodgers@xgs0r.onmicrosoft.com>
2025-01-01 14:56:27 -0300

    adicionando exemplo 02 com funções print e input

---
+++ exemplo_02.py
@@ -0,0 +1,12 @@
+#COMANDO PRINT# 
+print("Alguma Coisa") #mostra alguma coisa definida#
+
+print(3) #pode ser texto ou número#
+
+print(3 + 4) #pode ser opereações#
+
+
+print("Olá" + " " "Pessoal") #pode concatenar varáiveis do mesmo tippo#
+
+#COMANDO INPUT#
+print("Olá" + " " + input(" Digite o seu nome: ") + "!")
